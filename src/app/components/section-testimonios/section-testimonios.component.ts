82630141ccb292bf05d0eb9617af20b7f26c28fb
Guille <guillermogiacchetti85@gmail.com>
2024-10-14 18:24:33 -0300

    Preparando 28/09/2024 18:25

--- src/app/components/section-testimonios/section-testimonios.component.ts
+++ src/app/components/section-testimonios/section-testimonios.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { ScrollAnimationDirective } from '../scroll/scroll.animation';
+import { TestimoniosComponent } from '../testimonios/testimonios.component';
 
 @Component({
   selector: 'app-section-testimonios',
   standalone: true,
   imports: [
-    ScrollAnimationDirective
+    ScrollAnimationDirective,
+    TestimoniosComponent
   ],
   templateUrl: './section-testimonios.component.html',
   styleUrl: './section-testimonios.component.css'

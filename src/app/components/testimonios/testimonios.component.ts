b6af012b1e7f25739e0165d2468003fcbef2b972
Guille <guillermogiacchetti85@gmail.com>
2025-01-07 17:53:49 -0300

    Preparando 07/01/2025 17:53

--- src/app/components/testimonios/testimonios.component.ts
+++ src/app/components/testimonios/testimonios.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, ChangeDetectorRef, Component, Renderer2 } from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Component, ElementRef, Renderer2, ViewChild } from '@angular/core';
 import { TitleServices } from '../servicios/title.service';
 import { NgClass } from '@angular/common';
 
@@ -18,13 +18,30 @@ export class TestimoniosComponent implements AfterViewInit {
   constructor(private changeDtRef: ChangeDetectorRef, private titleService: TitleServices, private render: Renderer2) {} // constructor;
 
   public ngAfterViewInit(): void {
-
     this.titleService.Light.subscribe( (v) => {
       this.valor = Boolean(v);
     } ); // subscribe;
     this.changeDtRef.detectChanges();
+
+    this.ancho = this.Box.nativeElement.offsetWidth;
+    this.altura = this.Box.nativeElement.offsetHeight;
+    this.Top = this.Box.nativeElement.offsetHeight - this.Box.nativeElement.offsetHeight;
+    this.Center = this.ancho / 2;
   } // ngAfterViewInit();
 
+  public Top = 0;
+  public Left = 0;
+  public Center = 0;
+  public ancho = 0;
+  public altura = 0;  
+  public hi: string = "";
+  @ViewChild('Box', {static: false}) Box!: ElementRef;
+
+
+  public movemouse(): void {
+    // this.render.setStyle(this.Hi.nativeElement, "border", "2px solid cyan");
+    this.hi = "Hola";
+  } // movemouse();
 
 
 } // TestimoniosComponent;

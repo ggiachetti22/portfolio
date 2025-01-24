9804812f6ea2ecfe5821e9e9204146fc93aad4a7
Guille <guillermogiacchetti85@gmail.com>
2025-01-24 17:46:43 -0300

    Preparando 24/01/2025 17:45

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -333,7 +333,7 @@ export class NavComponent implements OnInit {
   } // this.ResizeSlider($event);
  
 
-  protected myScroll() { // (window:scroll)="myScroll();" // de forma dinámica;
+  /* protected myScroll() { // (window:scroll)="myScroll();" // de forma dinámica;
     try {
       const footer = document.querySelector('.footer') as HTMLElement;
       const NavSection = document.getElementById('NavSection');
@@ -355,8 +355,6 @@ export class NavComponent implements OnInit {
         } else {
           this.renderer.setStyle(this.parrafoBox.nativeElement, 'right', '-40px');
         } // else;
-
-        this.renderer.setStyle(this.parrafoBox.nativeElement, 'right', '-40px');
   
         if (this.PositionInitial >= PositionActual) {
           this.renderer.setStyle(NavSection, 'opacity', null);
@@ -370,7 +368,36 @@ export class NavComponent implements OnInit {
     } catch (error) {
       console.error('Error en myScroll:', error);
     }
-  } // myScroll();
+  } // myScroll(); */
+
+ 
+
+  protected myScroll() { // (window:scroll)="myScroll2();" // de forma dinámica;
+    const footer = document.querySelector('.footer') as HTMLElement;
+    const NavSection = document.getElementById('NavSection');
+    let BodyHeight = document.body.offsetHeight; 
+    let FooterHeight = footer.offsetHeight;
+    let BoxUp = BodyHeight - (FooterHeight + 400);
+    let PositionActual = window.scrollY;
+
+    if (footer && NavSection && BodyHeight && FooterHeight && BoxUp && PositionActual) {
+      // console.log(`BodyHeight: ${BodyHeight}\nFooterHeight: ${FooterHeight}`);
+      if(this.PositionInitial >= BoxUp) this.renderer.setStyle(this.parrafoBox.nativeElement, 'right', null);
+      else this.renderer.setStyle(this.parrafoBox.nativeElement, 'right', '-40px');
+
+      if (this.PositionInitial >= PositionActual) {
+        this.renderer.setStyle(NavSection, 'opacity', null);
+        NavSection?.classList.remove('NavToggle');
+      } else {
+        if (this.PositionInitial >= 60) this.renderer.setStyle(NavSection, 'opacity', '0');
+        NavSection?.classList.add('NavToggle');
+      } // else;
+
+      this.PositionInitial = PositionActual;
+
+    } // if;
+
+  }; // myScroll();
 
 
   

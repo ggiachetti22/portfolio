8a972f14c7d4cec7ac2f722d1b0c62dad7f1cfd4
Guille <guillermogiacchetti85@gmail.com>
2025-01-24 17:40:21 -0300

    Preparando 24/01/2025 17:40

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -335,23 +335,24 @@ export class NavComponent implements OnInit {
 
   protected myScroll() { // (window:scroll)="myScroll();" // de forma dinámica;
     try {
-      // const footer = document.querySelector('.footer') as HTMLElement;
+      const footer = document.querySelector('.footer') as HTMLElement;
       const NavSection = document.getElementById('NavSection');
-  
-      if (!NavSection) {
+
+      let BodyHeight = document.body.offsetHeight; 
+      let FooterHeight = footer.offsetHeight;
+      
+      if (!footer || !NavSection) {
         console.warn('Elementos requeridos no están disponibles en el DOM.');
+        FooterHeight = 600;
         return;
       } // if;
   
-      // let BodyHeight = document.body.offsetHeight; 
-      // let FooterHeight = footer.offsetHeight;
-      // let BoxUp = BodyHeight - (FooterHeight + 400);
-
+      let BoxUp = BodyHeight - (FooterHeight + 400);
       let PositionActual = window.scrollY;
   
       if (PositionActual !== undefined) {
 
-        if (this.PositionInitial >= 800) { // this.PositionInitial >= BoxUp
+        if (this.PositionInitial >= BoxUp) {
           this.renderer.setStyle(this.parrafoBox.nativeElement, 'right', null);
         } else {
           this.renderer.setStyle(this.parrafoBox.nativeElement, 'right', '-40px');

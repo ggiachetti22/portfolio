7ff4c7c2623a070e46c0b3dc579f956dfe3007c2
ggiachetti22 <104290704+ggiachetti22@users.noreply.github.com>
2025-02-25 22:03:34 -0300

    Update banner.component.ts

--- src/app/components/banner/banner.component.ts
+++ src/app/components/banner/banner.component.ts
@@ -12,7 +12,7 @@ import { Component } from '@angular/core';
 })
 export class BannerComponent {
 
-  public banner: String[] = ['HTML', 'CSS', 'JavaScript', 'Angular', 'C# Sharp', '.Net Core', 'Sql Server', 'GitHub', 'AWS', 'Azure'];
+  public banner: String[] = ['HTML', 'CSS', 'JavaScript', 'Angular', 'C#Sharp', '.Net Core', 'Sql Server', 'GitHub', 'AWS', 'Azure'];
   public banner2: String[] = ['icons8-html-5-96.png', 'icons8-css-96.png', 'icons8-javascript-96.png', 'icons8-angularjs-96.png', 'icons8-c-sharp-logotipo-96.png', 'icons8-net-framework-96.png', 'icons8-servidor-microsoft-sql-96.png', 'icons8-github-96.png', 'icons8-aws-96.png', 'icons8-azur-96.png'];
   public conbiedArray = this.banner.map((a, i) => ({a, b: this.banner2[i]}));
 
